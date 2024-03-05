047c9c09f53e54f7cc04be3de90dc31f0dc0cba4
starfaby <star._faby@hotmail.com>
2024-03-05 11:44:21 -0500

    Detalle venta

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ def notfound(error):
 def internalserveererror(error):
     return render('errors/error500.html')
 
+
 @app.route('/')
 def index():  
     return render('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- src/client/controller/clientControllerDetalleNotaVenta.py
+++ src/client/controller/clientControllerDetalleNotaVenta.py
@@ -1,16 +1,9 @@
 # ingresar al sistema ya registrado
 from flask import request, render_template as render , flash, redirect, url_for
 from flask_login import login_user, logout_user, login_required
-from src.client.services.clientServiceNotaVentaSerial import ClientServiceNotaVentaSerial
-from src.client.services.clientServiceNotaVentaCreate import ClientServiceNotaVentaCreate
 from src.client.services.clientServiceNumNotaVentaUser import ClientServiceNumNotaVentaUser
 from src.client.services.clientServiceDetalleNotaVenta import ClientServiceDetalleNotaVenta
-from src.middlewares.middlewaresLoginIn import UserModel
-from sqlalchemy.exc import SQLAlchemyError
-from src.auth.security.securityAuth import SecurityAuth
-import numpy as np
 from datetime import datetime
-from flask_login import current_user
 
 class ClientControllerDetalleNotaVenta():
     
@@ -18,6 +11,7 @@ class ClientControllerDetalleNotaVenta():
         idCanasta = 0
         cnstNum = 0
         auxIdNumUser = ClientServiceNumNotaVentaUser.onGetClientServiceNumNotaVentaUser()
+        #detalleNoVe = ClientServiceDetalleNotaVenta.onGetClientServiceDetalleNotaVentaAll()
         for item in auxIdNumUser:
             idCanasta = item.pfsabcnstid
             cnstNum = item.pfsabcnstnumpf
@@ -32,8 +26,6 @@ class ClientControllerDetalleNotaVenta():
                 pfsabdcreatedat = datetime.now()
                 pfsabproductoid = request.form['txtIdProducto']
                 pfsabcanastaid = idCanasta
-                
-
 
                 if pfsabdcnumpf != '' and pfsabdcantidad != '' and pfsabdcprecio != '' and pfsabdctotal != '' and pfsabdcestado != '' and  pfsabdcreatedat != '' and pfsabproductoid != '' and pfsabcanastaid != '':
                     ClientServiceDetalleNotaVenta.onGetClientServiceDetalleNotaVentaSave(pfsabdcnumpf, pfsabdcantidad, pfsabdcprecio, pfsabdctotal, pfsabdcestado, pfsabdcreatedat, pfsabproductoid, pfsabcanastaid)
